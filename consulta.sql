2027aaa49a740431f36c8645e65767615c410740
vitorjoaorr <167055412+vitorjoaorr@users.noreply.github.com>
2026-08-01 14:45:59 -0300

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -1,28 +1,28 @@
---1
+--1 consulta de preço do produto
 select nome_produto, preco
 from produto
 where preco > 100;
 
 
---2
-select nome_produto, estoque
-from produto
-where estoque < 20;
+--2 buscar cliente
+select nome_cliente
+from cliente
+where nome_cliente ilike 'joão';
 
 
---3
+--3 produtos cadastrados
 select count(id_produto) as quantidade_produtos
 from produto;
 
 
---4
+--4 quantidade de produtos por categoria
 select id_categoria, count(id_produto) AS quantidade_produtos
 from produto
 group by id_categoria
 order by id_categoria asc;
 
 
---5
+--5 registro do pedido com informações sobre cliente, data e status do pedido
 select
     p.id_pedido,
     c.nome_cliente,
@@ -32,7 +32,7 @@ from pedido p
 join cliente c on c.id_cliente = p.id_cliente;
 
 
---6
+--6 detalhes do pedido (produtos, quantidade e valor unitario)
 select
     ped.id_pedido,
     prod.nome_produto,
@@ -43,7 +43,7 @@ join pedido_produto pp on pp.id_pedido = ped.id_pedido
 join produto prod on prod.id_produto = pp.id_produto;
 
 
---7
+--7 consulta do status do pedido de cada cliente (incluindo null)
 select
     c.nome_cliente,
     p.id_pedido,
@@ -52,7 +52,7 @@ from cliente c
 left join pedido p on p.id_cliente = c.id_cliente;
 
 
---8
+--8 tabela de pedidos com valor total da compra sendo calculada
 select
     pp.id_pedido,
     sum(pp.quantidade * pp.preco_unitario) AS valor_total
@@ -61,7 +61,7 @@ group by pp.id_pedido
 order by pp.id_pedido asc;
 
 
---9
+--9 calcula a media dos preços e exibe apenas os que estao acima desta media
 with media_produtos as (
     select avg(preco) as media
     from produto
@@ -71,7 +71,7 @@ from produto
 where preco > (select media from media_produtos);
 
 
---10
+--10 cria um rank e organiza do produto mais caro ao mais barato
 select nome_produto, preco,
 rank() over (order by preco desc) as ranking_preco
 from produto;
